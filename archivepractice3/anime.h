4520b9f0dc8e87b460f9ba0fa2b7b05d353bbe7c
Katherine Blanco Miranda <katherine.blanco.miranda@est.una.ac.cr>
2023-10-31 20:21:56 -0700

    Create project archivepractice
    Create Anime class
    
    Add Anime class atributes
    
    Create UML diagram archivepracticeUML

---
+++ archivepractice3/anime.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Anime {
+
+public:
+
+	string name;
+	int year;
+	string author;
+	string code;
+	string description;
+	string path;
+
+};
